8bb32429e1d5b4b9c2032dae00ef54b216f3cd01
Jesse <nguyenjessev@gmail.com>
2020-08-09 12:18:18 -0400

    Add Human methods to read user input

--- mastermind.rb
+++ mastermind.rb
@@ -1,10 +1,34 @@
 # frozen_string_literal: true
 
 module Mastermind
+  COLORS = %w[red blue green yellow black white].freeze
+
   class Game
   end
 
+  # This class represents a human player
   class Human
+    def input_code
+      puts 'Please enter your code:'
+      4.times do |num|
+        print "Position #{num}: "
+        input = input_color
+      end
+    end
+
+    private
+
+    def input_color
+      loop do
+        input = gets.chomp.downcase
+
+        break if @@COLORS.include?(input)
+
+        puts 'Invalid input, please try again:'
+      end
+
+      input
+    end
   end
 
   class Cpu
