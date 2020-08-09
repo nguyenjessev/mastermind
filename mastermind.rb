5f23eff95194072ca4bd21db9f8f1b55633b27bd
Jesse <nguyenjessev@gmail.com>
2020-08-09 11:57:59 -0400

    Add Code initialize method

--- mastermind.rb
+++ mastermind.rb
@@ -10,6 +10,12 @@ module Mastermind
   class Cpu
   end
 
+  # This class represents a 4-color code
   class Code
+    attr_accessor :code
+
+    def initialize
+      @code = []
+    end
   end
 end
